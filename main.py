7848acb2963336f019e843798dfa7f6fcc040db7
CtREL0K <Kzuev905@gmail.com>
2025-07-09 23:32:26 +0500

    qwdqwqdw

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origin_regex=r"https://(.*\.)?vercel\.app",
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
